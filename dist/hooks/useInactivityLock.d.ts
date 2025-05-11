855cb218df95a903261da3fe72935e923000b58b
Matthew Nolan <mnolan132@gmail.com>
2025-05-12 09:34:48 +1200

    Update export file

---
+++ dist/hooks/useInactivityLock.d.ts
@@ -0,0 +1,7 @@
+type InactivityLockOptions = {
+    timeout: number;
+    onLock: () => void;
+};
+export declare function useInactivityLock({ timeout, onLock }: InactivityLockOptions): null;
+export {};
+//# sourceMappingURL=useInactivityLock.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ export { SecureFileUpload } from './components/secureFileUpload/SecureFileUpload
 // hooks
 export { useSanitizedForm } from './hooks/useSanitizedForm';
 export { useSecureLocalStorage } from './hooks/useSecureLocalStorage';
+export { useInactivityLock } from './hooks/useInactivityLock';
 
 //types
 export type { SecureTextInputProps } from './SecureTextInput'; // if you're using TypeScript types
